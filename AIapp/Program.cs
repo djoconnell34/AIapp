40a8ef210f186790b25930bb0d053200ac987cfc
Daniel O'Connell <djoconnell34@gmail.com>
2022-05-18 14:31:36 -0400

    Add project files.

---
+++ AIapp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<HousingData>("C:/Users/djoco/OneDrive/Documents/C#/CapstoneNew/AIapp/AIapp/housing.csv", hasHeader: true,
+                separatorChar: ',');
+
+            var spilt = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+            var features = spilt.TrainSet.Schema
+                .Select(col => col.Name)
+                .Where(colName => colName != "Label" && colName != "OceanProximity")
+                .ToArray();
+
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Transforms.Concatenate("Feature", "Features", "Text"))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+            var model = pipeline.Fit(spilt.TrainSet);
+
+            var predictions = model.Transform(spilt.TestSet);
+
+            var metrics = context.Regression.Evaluate(predictions);
+
+            Console.WriteLine($"R^2 - {metrics.RSquared}");
+        }
+    }
+}
